daa51cd77132154335e728c2694bf43f4564d280
kinpkt <phakhinthon@gmail.com>
2026-02-26 21:42:15 +0700

    Add course creation feature

--- lib/src/features/student/courses/data/models/course_model.dart
+++ lib/src/features/student/courses/data/models/course_model.dart
@@ -2,28 +2,68 @@ import 'package:uuid/uuid.dart';
 import 'course_schedule_model.dart';
 
 class CourseModel {
-  final String uid;
+  final String id;
   final String name;
   final String? description;
-  final double? credit;
+  // final double? credit;
   final String userId;
   final String? teacherName;
   final List<CourseScheduleModel> schedule;
   final DateTime createdAt;
   final DateTime updatedAt;
+  final bool isActive;
+  DateTime? inactiveDateTime;
 
   CourseModel({
-    String? uid,
+    String? id,
     required this.name,
     this.description,
-    this.credit,
     required this.userId,
     this.teacherName,
     List<CourseScheduleModel>? schedule,
     DateTime? createdAt,
     DateTime? updatedAt,
-  })  : uid = uid ?? const Uuid().v7(),
+    required this.isActive,
+    DateTime? inactiveDateTime
+  })  : id = id ?? const Uuid().v7(),
         schedule = schedule ?? [],
         createdAt = createdAt ?? DateTime.now(),
         updatedAt = updatedAt ?? DateTime.now();
+
+  factory CourseModel.fromFirestore(Map<String, dynamic> data, String docId) {
+    return CourseModel(
+      id: docId,
+      name: data['name'] as String,
+      description: data['description'] as String?,
+      isActive: data['isActive'] as bool,
+      userId: data['userId'] as String,
+      teacherName: data['teacherName'] as String?,
+      schedule: (data['schedule'] as List<dynamic>?)?.map(
+        (scheduleData) => CourseScheduleModel.fromFirestore(
+          scheduleData as Map<String, dynamic>
+        )
+      ).toList(),
+      createdAt: data['createdAt'].toDate(),
+      updatedAt: data['updatedAt'].toDate(),
+      inactiveDateTime: data['inactiveDateTime'] != null ? data['inactiveDateTime'].toDate() : null,
+    );
+  }
+
+  Map<String, dynamic> toFirestore() {
+    return {
+      'id': id,
+      'name': name,
+      if (description != null)
+        'description': description!,
+      'userId': userId,
+      if (teacherName != null)
+        'teacherName': teacherName!,
+      'schedule': schedule.map(
+        (sch) => sch.toFirestore()
+      ).toList(),
+      'createdAt': createdAt,
+      'updatedAt': updatedAt,
+      'isActive': isActive
+    };
+  }
 }
--- lib/src/features/student/courses/data/models/course_schedule_model.dart
+++ lib/src/features/student/courses/data/models/course_schedule_model.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:studall/src/core/utils/time_conversion.dart';
 
 enum DayOfWeek {
   monday,
@@ -12,8 +13,30 @@ enum DayOfWeek {
 
 class CourseScheduleModel {
   DayOfWeek day;
+  String? location;
   TimeOfDay? startTime;
   TimeOfDay? endTime;
 
-  CourseScheduleModel({required this.day, this.startTime, this.endTime});
+  CourseScheduleModel({required this.day, this.location, this.startTime, this.endTime});
+
+  factory CourseScheduleModel.fromFirestore(Map<String, dynamic> data) {
+    return CourseScheduleModel(
+      day: DayOfWeek.values.byName(data['day']),
+      location: data['location'] as String?,
+      startTime: data['startTime'] != null ? minuteToTimeOfDay(data['startTime']) : null,
+      endTime: data['endTime'] != null ? minuteToTimeOfDay(data['endTime']) : null,
+    );
+  }
+
+  Map<String, dynamic> toFirestore() {
+    return {
+      'day': day.name,
+      if (location != null)
+        'location': location,
+      if (startTime != null)
+        'startTime': timeOfDayToMinute(startTime!),
+      if (endTime != null)
+        'endTime': timeOfDayToMinute(endTime!),
+    };
+  }
 }
